5b13d4c7155519e5ac0f75a236e56659cb85d416
DeafMan1983 <57066679+DeafMan1983@users.noreply.github.com>
2022-04-27 14:28:33 +0100

    This ist update

---
+++ 05_Matrix_A_Transformation/src/Example/VertexArrayObject.java
@@ -0,0 +1,30 @@
+package Example;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL30;
+
+public class VertexArrayObject
+{
+    private int vaoID;
+    public static VertexArrayObject Gen()
+    {
+        var vaoClass = new VertexArrayObject();
+        vaoClass.vaoID = GL30.glGenVertexArrays();
+        return vaoClass;
+    }
+    
+    public void Bind()
+    {
+        GL30.glBindVertexArray(this.vaoID);
+    }
+    
+    public void Unbind()
+    {
+        GL30.glBindVertexArray(0);
+    }
+    
+    public void Delete()
+    {
+        GL30.glDeleteVertexArrays(this.vaoID);
+    }
+}
